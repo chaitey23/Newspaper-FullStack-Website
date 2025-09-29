ab7dfd4c0697bb1caf8a64acff2acb51d0b53f4d
chaitey50 <chaiteychaitey30@gmail.com>
2025-09-29 16:27:05 +0600

    add ScrollToTop for route change scroll reset

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 import Footer from '../Pages/Shared/Navbar/Footer/Footer';
+import ScrollToTop from '../Pages/ScrollToTop/ScrollToTop';
 
 const MainLayout = () => {
     return (
         <div className='min-h-screen flex flex-col'>
             <Navbar></Navbar>
+            <ScrollToTop></ScrollToTop>
             <div className='w-11/12 mx-auto p-4'>
                 <Outlet></Outlet>
             </div>
